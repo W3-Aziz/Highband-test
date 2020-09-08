1fef2be0217f51c4fe2c7a71ccf4a57fdd700a33
Aziz <azizul@w3engineers.com>
2020-09-08 13:41:45 +0600

    single object

--- app/src/main/java/com/w3engineers/highbandtest/ProtocolManager.java
+++ app/src/main/java/com/w3engineers/highbandtest/ProtocolManager.java
@@ -1,4 +1,18 @@
 package com.w3engineers.highbandtest;
 
+import android.content.Context;
+
 class ProtocolManager {
+    private static ProtocolManager protocolManager;
+    private Context mContext;
+    private ProtocolManager(Context context){
+        this.mContext = context;
+    }
+
+    public ProtocolManager on(Context context){
+        if(protocolManager != null){
+            protocolManager = new ProtocolManager(context);
+        }
+        return protocolManager;
+    }
 }
